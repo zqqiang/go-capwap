76d01935ed58cd299d64f992ec4ce187df0e3fe9
zqqiang <qiangzhaoqing@gmail.com>
2018-01-12 13:14:36 -0800

    add cfg fos module

---
+++ fos/fos.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":5246")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go func(c net.Conn) {
+			io.Copy(c, c)
+			c.Close()
+		}(conn)
+	}
+}
