5ff209f2327ef9184bfd617575b3233538971cc5
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2018-03-21 09:33:02 -0700

    add preamble

--- ap/ap.go
+++ ap/ap.go
@@ -9,9 +9,31 @@ import (
 )
 
 type (
-	discoveryReq struct {
+	// Preamble Preamble
+	Preamble struct {
+		preamble uint8
+	}
+
+	// DiscoveryRequest Discovery Request
+	DiscoveryRequest struct {
+		preamble      Preamble
 		discoveryType uint8
 	}
+)
+
+// SetVersion Set Preamble Version
+func (p *Preamble) SetVersion(version uint8) {
+	p.preamble |= version << 4
+}
+
+// GetVersion Get Preamble Version
+func (p *Preamble) GetVersion() uint8 {
+	return p.preamble >> 4
+}
+
+const (
+	version      = 0
+	capwapHeader = 0
 )
 
 const (
@@ -20,7 +42,11 @@ const (
 )
 
 func cwWtpEnterDiscovery() {
-	r := discoveryReq{
+	p := Preamble{}
+	p.SetVersion(version)
+
+	r := DiscoveryRequest{
+		preamble:      p,
 		discoveryType: cwMsgElementDiscoveryTypeConfigured,
 	}
 	buf := &bytes.Buffer{}
