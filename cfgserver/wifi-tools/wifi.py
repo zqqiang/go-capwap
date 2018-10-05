a86d97e3fa620c278af7ce1c2dddc85cfcfd05d7
zqqiang <qiangzhaoqing@gmail.com>
2018-10-05 07:03:42 -0700

    update wifi tool

--- cfgserver/wifi-tools/wifi.py
+++ cfgserver/wifi-tools/wifi.py
@@ -1,17 +1,30 @@
 import xml.etree.ElementTree as ET
+from string import Template
 
 tree = ET.parse('mgmt-data.xml')
 root = tree.getroot()
 
-platforms = root.findall('.//cw_platform_type/platform')
+# platforms = root.findall('.//cw_platform_type/platform')
 
-for platform in platforms:
-    print(platform.tag, platform.attrib)
+# for platform in platforms:
+#     print(platform.tag, platform.attrib)
 
-wtpcaps = root.findall('.//cw_wtp_cap/wtpcap')
+# wtpcaps = root.findall('.//cw_wtp_cap/wtpcap')
 
-for wtpcap in wtpcaps:
-    print(wtpcap.tag, wtpcap.attrib)
-    radios = wtpcap.findall('radio')
-    for radio in radios:
-        print(radio.tag, radio.attrib)
+# for wtpcap in wtpcaps:
+#     print(wtpcap.tag, wtpcap.attrib)
+#     radios = wtpcap.findall('radio')
+#     for radio in radios:
+#         print(radio.tag, radio.attrib)
+
+
+def buildWifiPlatformSql():
+    platforms = root.findall('.//cw_platform_type/platform')
+    for platform in platforms:
+        # print(platform.tag, platform.attrib)
+        line = Template('($captype, "$name", "$help"),').substitute(
+            platform.attrib)
+        print(line)
+
+
+buildWifiPlatformSql()
