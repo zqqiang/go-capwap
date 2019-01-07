e2a448093b2de0391b4352b6e8cce5d4ff83be89
zqqiang <qiangzhaoqing@gmail.com>
2019-01-07 08:34:59 -0800

    clean code

--- kms/src/components/Home.js
+++ kms/src/components/Home.js
@@ -1,25 +1,8 @@
 import React from "react";
-import TopNavigation from "./TopNavigation";
-import Footer from "./Footer";
 import { Switch, Route } from "react-router-dom";
-import Table from "./Table";
 
 export default class Home extends React.Component {
   render() {
-    return (
-      <div>
-        <section className="hero is-fullheight">
-          <TopNavigation />
-          <div className="hero-body">
-            <div className="container">
-              <Switch>
-                <Route path="/" component={Table} />
-              </Switch>
-            </div>
-          </div>
-          <Footer />
-        </section>
-      </div>
-    );
+    return <div>Home Page</div>;
   }
 }
--- kms/src/components/Table.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Table extends React.Component {
-  render() {
-    return <div>Table</div>;
-  }
-}
-
-export default Table;
